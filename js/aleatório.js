8457af291fee3bcd896425a4e730459e46f3e757
Mariawx <grzeszcyszen.maria@escola.pr.gov.br>
2026-09-24 09:24:21 -0300

    .

--- js/aleatório.js
+++ js/aleatório.js
@@ -1,7 +1,4 @@
 export function aleatorio(lista) {
-    if (Array.isArray(lista)) {
         const posicao = Math.floor(Math.random() * lista.length);
         return lista[posicao];
     }
-    return lista;
-}
